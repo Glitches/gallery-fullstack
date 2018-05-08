620cfc4a2eca87afe1a51e793fa5eed1dc8cb4aa
Andrea Ceccarelli <gltch2003@gmail.com>
2018-05-08 16:32:27 +0200

    feature: client first commit

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import RenderDOM from 'react-dom';
+import App from './src/app';
+
+RenderDOM.render(<App />, 'root');
---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  render() {
+    return(
+      <div>
+        <h1>HELLO</h1>
+    )
+  }
+}
