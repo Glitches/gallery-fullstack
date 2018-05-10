9d9ec4cbaf187054002ffe4c7bdb82d2ccee853e
Andrea Ceccarelli <gltch2003@gmail.com>
2018-05-10 14:38:24 +0200

    feature: get data from flickr API and send to client photolist

--- server/router.js
+++ server/router.js
@@ -1,10 +1,22 @@
 'use strict';
-
+const axios = require('axios');
 const Express = require('express');
 const router = Express.Router();
 
-router.get('/static', (req, res, next) => {
-  res.send('hello');
+router.get('/getPhotoList', async (req, res, next) => {
+  const response = await axios.get(
+    'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=8847401c31517199c53b73ed96da09e4&page=1&format=json&nojsoncallback=1'
+  );
+  const urlList = response.data.photos.photo.map(
+    el =>
+      `https://farm${el.id}.staticflickr.com/${el.farm}/${el.id}_${
+        el.secret
+      }_m.jpg`
+  );
+  console.log(urlList);
+  // const data = await response.json();
+  res.send({ urlList });
+  // res.send(200);
   next();
 });
 
