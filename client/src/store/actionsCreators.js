e4c83f7fbb07ce45831f79bafcd75d0457746e23
Andrea Ceccarelli <gltch2003@gmail.com>
2018-05-11 14:51:50 +0200

    chore: use fetch instead of rxjs ajax

--- client/src/store/actionsCreators.js
+++ client/src/store/actionsCreators.js
@@ -1,8 +1,7 @@
 import { FETCH_THUMBNAILS, FETCH_THUMBNAILS_FULFILLED } from './actions';
 
-export const fetchThumbnails = username => ({
-  type: FETCH_THUMBNAILS,
-  payload: username
+export const fetchThumbnails = () => ({
+  type: FETCH_THUMBNAILS
 });
 export const fetchThumbnailsFulfilled = payload => ({
   type: FETCH_THUMBNAILS_FULFILLED,
--- client/src/store/epics.js
+++ client/src/store/epics.js
@@ -5,9 +5,12 @@ import { fetchThumbnails, fetchThumbnailsFulfilled } from './actionsCreators';
 import { FETCH_THUMBNAILS } from './actions';
 
 export const fetchThumbnailsEpic = action$ =>
-  action$.ofType(FETCH_THUMBNAILS).mergeMap(action =>
-    // eslint-disable-next-line no-undef
-    ajax
-      .getJSON(`https://api.github.com/users/${action.payload}`)
-      .map(response => fetchThumbnailsFulfilled(response))
-  );
+  action$
+    .ofType(FETCH_THUMBNAILS)
+    .mergeMap(action =>
+      // eslint-disable-next-line no-undef
+      fetch('http://localhost:5000/getPhotoList')
+        .then(response => response.json())
+        .then(data => fetchThumbnailsFulfilled(data))
+    )
+    .catch(error => console.log(error));
