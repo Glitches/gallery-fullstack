56ce6b3d4bb84ad8ce584d6a5bc585362b7e1a39
Andrea Ceccarelli <gltch2003@gmail.com>
2018-05-11 14:52:31 +0200

    chore: add try catch to server route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const Express = require('express');
 const app = Express();
+const cors = require('cors');
 
 const router = require('./router');
-app.use('/', Express.static(__dirname + '/view')).use(router.router);
+app
+  .use(cors())
+  .use('/', Express.static(__dirname + '/view'))
+  .use(router.router);
 
 module.exports = { app };
--- server/router.js
+++ server/router.js
@@ -1,21 +1,26 @@
-'use strict';
-const axios = require('axios');
-const Express = require('express');
+"use strict";
+const axios = require("axios");
+const Express = require("express");
 const router = Express.Router();
 
-router.get('/getPhotoList', async (req, res, next) => {
-  const response = await axios.get(process.env.URL);
-  const urlList = response.data.photos.photo.map(
-    el =>
-      `https://farm${el.id}.staticflickr.com/${el.farm}/${el.id}_${
-        el.secret
-      }_m.jpg`
-  );
-  console.log(urlList);
-  // const data = await response.json();
-  res.send({ urlList });
-  // res.send(200);
-  next();
+router.get("/getPhotoList", async (req, res, next) => {
+  try {
+    const response = await axios.get(process.env.URL);
+    console.log(response);
+    const urlList = response.data.photos.photo.map(
+      el =>
+        `https://farm${el.farm}.staticflickr.com/${el.server}/${el.id}_${
+          el.secret
+        }_m.jpg`
+    );
+    console.log(urlList);
+    // const data = await response.json();
+    res.send({ urlList });
+    // res.send(200);
+    next();
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
 });
 
 module.exports = { router };
